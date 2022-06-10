4bcce20a00fb0b555400a7149770f86c8d11b35d
21a91a0560 <97602364+21a91a0560@users.noreply.github.com>
2022-06-10 22:47:41 +0530

    a=int(input()) b=int(input()) n=a+b org=n temp=n+1 while n>0:  for i in range(2,int(temp**0.5)+1):    if temp%i==0:       break;  else:    k=temp    break  temp=temp+1 print(abs(k-org))

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,17 +1,14 @@
-def is_prime(k):
- for i in range(2,int(k**0.5)+1):
-   if k%i==0:
-      return 0
- else:
-   return 1
 a=int(input())
 b=int(input())
 n=a+b
-k=n+1
-s=0
-while k>1:
- if is_prime(k):
-     s=k
-     break
- k=k+1
-print(s-n)
+org=n
+temp=n+1
+while n>0:
+ for i in range(2,int(temp**0.5)+1):
+   if temp%i==0:
+      break;
+ else:
+   k=temp
+   break
+ temp=temp+1
+print(abs(k-org))

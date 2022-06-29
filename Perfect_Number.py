1c61308826b33bd290099a630752e9dcd52e8ed4
21a91a0560 <97602364+21a91a0560@users.noreply.github.com>
2022-06-29 07:17:57 +0530

    def none(n):     su=0     for i in range(1,n):         if(n%i==0):             su+=i     return su a=int(input()) if(none(a)==a):     print("True") else:     print("False")

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,9 +1,11 @@
-n=int(input())
-ad=0
-for i in range(1,n//2+1):
-  if n%i==0:
-     ad=ad+i
-if ad==n:
-   print('True')
+def none(n):
+    su=0
+    for i in range(1,n):
+        if(n%i==0):
+            su+=i
+    return su
+a=int(input())
+if(none(a)==a):
+    print("True")
 else:
-   print('False')
+    print("False")

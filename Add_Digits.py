899375dcc62b452d4a7385e9ab85cda6c9ba0fc1
21a91a0560 <97602364+21a91a0560@users.noreply.github.com>
2022-06-29 07:15:50 +0530

    def suma(a):     temp=a     su=0     while True:         su=0         while(temp>0):             r=temp%10             su+=r             temp=temp//10         if(su<10):             return su
            else:
                temp=su
                su=0
                continue
    a=int(input())
    de=suma(a)
    print(de)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,13 +1,19 @@
-def is_sum(n):
-    s=0
-    while n:
-        d=n%10
-        s=s+d
-        n=n//10
-    if s<=9:
-        return s
-    else:
-        n=is_sum(s)
-        return n
-n=int(input())
-print(is_sum(n))
+def suma(a):
+    temp=a
+    su=0
+    while True:
+        su=0
+        while(temp>0):
+            r=temp%10
+            su+=r
+            temp=temp//10
+        if(su<10):
+            return su
+            
+        else:
+            temp=su
+            su=0
+            continue
+a=int(input())
+de=suma(a)
+print(de)

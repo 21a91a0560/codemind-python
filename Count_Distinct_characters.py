edb134f5954b5c8f7a1abd8dd59b5d0a4a537cc3
21a91a0560 <97602364+21a91a0560@users.noreply.github.com>
2022-06-29 07:12:06 +0530

    a=input() arr=[] for i in a:     if i.isspace():         continue     else:         if i.lower() not in arr:             arr.append(i.lower()) for i in range(len(arr)):     for j in range(len(arr)-1):         if arr[j]>arr[j+1]:             arr[j],arr[j+1]=arr[j+1],arr[j] print(len(arr))

---
+++ Count_Distinct_characters.py
@@ -0,0 +1,13 @@
+a=input()
+arr=[]
+for i in a:
+    if i.isspace():
+        continue
+    else:
+        if i.lower() not in arr:
+            arr.append(i.lower())
+for i in range(len(arr)):
+    for j in range(len(arr)-1):
+        if arr[j]>arr[j+1]:
+            arr[j],arr[j+1]=arr[j+1],arr[j]
+print(len(arr))

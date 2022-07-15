c2e2648847bc54e022e129a1a60a8fa484c02044
21a91a0560 <97602364+21a91a0560@users.noreply.github.com>
2022-07-15 18:55:21 +0530

    def prime(n):     for i in range(2,n):         if n%i==0:             return 0     else:         return 1 n=int(input()) r=0 if prime(n):     while n:         d=n%10         n//=10         r=r*10+d     if prime(r):         print("circular prime")     else:         print("prime but not a circular prime") else:     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,22 +1,19 @@
 def prime(n):
-  if n==1:
-     return False
-  else:
-    for i in range(2,n//2+1):
-      if n%i==0:
-         return False
+    for i in range(2,n):
+        if n%i==0:
+            return 0
     else:
-      return True
+        return 1
 n=int(input())
+r=0
 if prime(n):
-   r=0
-   while n>0:
-    d=n%10
-    r=r*10+d
-    n=n//10
-   if prime(r):
-      print('circular prime')
-   else:
-     print('prime but not a circular prime')
+    while n:
+        d=n%10
+        n//=10
+        r=r*10+d
+    if prime(r):
+        print("circular prime")
+    else:
+        print("prime but not a circular prime")
 else:
-  print('not prime')
+    print("not prime")

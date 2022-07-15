30e7bf35082bc6040f17d288bffc1de02aa2c9fe
21a91a0560 <97602364+21a91a0560@users.noreply.github.com>
2022-07-15 18:51:30 +0530

    a,b=map(int,input().split()) k=a*b for i in range(a,k+1):  if i%a==0 and i%b==0:      print(i)      break

--- LCM.py
+++ LCM.py
@@ -1,5 +1,6 @@
 a,b=map(int,input().split())
-for i in range(a,a*b+1):
-    if i%a==0 and i%b==0:
-        break
-print(i)
+k=a*b
+for i in range(a,k+1):
+ if i%a==0 and i%b==0:
+     print(i)
+     break

3d5c7350278bc3bb2cc5f26ab6b7c4caffc04b2f
21a91a0560 <97602364+21a91a0560@users.noreply.github.com>
2022-07-15 19:14:48 +0530

    def prime(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     else:         return 1 n=int(input()) k=0 for i in range(2,(n//2)+1):     if prime(i):         for j in range(i+1,(n//2)+1):             if prime(j):                 if i*j==n:                     k=1                     print(i,j)                     break     if k==1:         break else:     print("-1")

--- Find_two_distinct_prime_numbers_with_given_product.py
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -1,21 +1,20 @@
-def is_prime(n):
- for i in range(2,int(n**0.5)+1):
-   if n%i==0:
-      return 0
- else:
-   return 1
+def prime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return 0
+    else:
+        return 1
 n=int(input())
-c=0
 k=0
-for i in range(1,n):
-  for j in range(1,n):
-    if i*j==n:
-       if is_prime(i) and is_prime(j):
-          print(i,j,end='')
-          c=c+1
-          break
-  if c==1:
-     k=k+1
-     break
-if k==0:
-   print('-1')
+for i in range(2,(n//2)+1):
+    if prime(i):
+        for j in range(i+1,(n//2)+1):
+            if prime(j):
+                if i*j==n:
+                    k=1
+                    print(i,j)
+                    break
+    if k==1:
+        break
+else:
+    print("-1")

e4cf70e018eb768eb7d82ccc7bbc5e00f42cd99d
21a91a0560 <97602364+21a91a0560@users.noreply.github.com>
2022-07-15 07:29:42 +0530

    n=int(input()) a=list(map(int,input().split())) for i in a:     if i==1 or i==0:         continue     else:         print("False")         break else:     print("True")

---
+++ Binary_Array.py
@@ -0,0 +1,12 @@
+n=int(input())
+a=list(map(int,input().split()))
+for i in a:
+    if i==1 or i==0:
+        continue
+    else:
+        print("False")
+        break
+else:
+    print("True")
+
+    
